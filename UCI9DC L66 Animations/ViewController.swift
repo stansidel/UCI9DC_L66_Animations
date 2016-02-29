fe0d9c0b979427293faac76424ce2d071cdea682
Stanislav Sidelnikov <sidelnikov.stanislav@gmail.com>
2016-02-29 12:33:51 +0500

    Automatic animations + toggling

--- UCI9DC L66 Animations/ViewController.swift
+++ UCI9DC L66 Animations/ViewController.swift
@@ -11,10 +11,12 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var alienImageView: UIImageView!
     var imageNumber = 1
+    var timer = NSTimer()
+    @IBOutlet weak var toggleAnimationButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        toggleAnimation(self)
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,7 +24,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func updateImage(sender: AnyObject) {
+    func doAnimation() {
         imageNumber += 1
         if imageNumber > 5 {
             imageNumber = 1
@@ -30,15 +32,26 @@ class ViewController: UIViewController {
         alienImageView.image = UIImage(named: "frame\(imageNumber).png")
     }
 
-    override func viewDidLayoutSubviews() {
-        alienImageView.frame = CGRect(x: 100, y: 20, width: 0, height: 0)
-    }
-
-    override func viewDidAppear(animated: Bool) {
-        UIView.animateWithDuration(1.0) { () -> Void in
-            self.alienImageView.frame = CGRect(x: 100, y: 20, width: 100, height: 200)
+    @IBAction func toggleAnimation(sender: AnyObject) {
+        if timer.valid {
+            timer.invalidate()
+            toggleAnimationButton.setTitle("Start Animation", forState: .Normal)
+        } else {
+            timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("doAnimation"), userInfo: nil, repeats: true)
+            toggleAnimationButton.setTitle("Stop Animation", forState: .Normal)
         }
     }
+
+//    override func viewDidLayoutSubviews() {
+//        alienImageView.frame = CGRect(x: 100, y: 20, width: 0, height: 0)
+//    }
+//
+//    override func viewDidAppear(animated: Bool) {
+//        UIView.animateWithDuration(1.0) { () -> Void in
+//            self.alienImageView.frame = CGRect(x: 100, y: 20, width: 100, height: 200)
+//        }
+//    }
+
 
 }
 
